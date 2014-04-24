8d7f6e9dc335df32d6e3817557bb64288db02f32
James Gilliland <jgilliland@apqc.org>
2014-04-24 16:04:12 -0500

    first commit

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             "apqc_php"
+maintainer       "James Gilliland"
+maintainer_email "jgilliland@apqc.org"
+license          "All rights reserved"
+description      "Installs/Configures PHP"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "1.0"
+
+depends "apache2"
